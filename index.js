834ba87a5d4ce840a87f0aff002da5a3d3164f9d
Oskari Noppa <the.nobba@gmail.com>
2018-04-10 17:45:13 +0300

    Create the asked files using predefined templates

--- index.js
+++ index.js
@@ -2,21 +2,61 @@ const args = require('process').argv.slice(2);
 const fs = require('fs');
 const path = require('path');
 
+// Helpers
+const isHtmlFile = file => file.ext === 'html';
+const isIndexFile = file => file.filename === 'index';
+const negate = fn => x => !fn(x);
+const writeFile = (pathname, file) => new Promise((resolve, reject) => {
+  fs.writeFile(pathname, file, (err) => {
+    if (err) reject(err);
+    else resolve();
+  });
+});
+
 const
   defaultOptions = {
-    scriptType: 'script'
+    scriptType: 'script',
+    title: 'title',
+    dir: '.'
   },
   handlers = {
-    html (opts, fileName) {
-
+    html (opts, filename, otherResults = []) {
+      const injectables = otherResults.map(result => result.inject).sort();
+      const template =
+`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>${opts.title}</title>${injectables.map(tag => `
+    ${tag}`).join('')}
+  </head>
+  <body>
+      <h1>Hello World!</h1>
+  </body>
+</html>
+`;
+      return { template, inject: undefined, filename };
     },
-    js (opts, fileName) {
+    js (opts, filename) {
+      const template = `console.log('Hello World!');` + '\n';
+      let { scriptType } = opts;
+      if (scriptType !== 'module') {
+        scriptType = 'text/javascript';
+      }
+      const inject = `<script type="${scriptType} src="${filename}"></script>`;
+      return { template, inject, filename };
     },
-    mjs (opts, fileName) {
-      return handlers.js({...opts, ...{ scriptType: 'module' }});
+    mjs (opts, ...rest) {
+      return handlers.js({...opts, ...{ scriptType: 'module' }}, ...rest);
     },
-    css () {
-
+    css (opts, filename) {
+      const template = `
+h1 {
+  text-align: center;
+  font-family: Verdana, sans-serif;
+}`;
+      const inject = `<link rel="stylesheet" type="text/css" href="${filename}"/>`;
+      return { template, inject, filename };
     }
   };
 
@@ -26,8 +66,10 @@ const
   fileRegex = new RegExp(
     `^([a-zA-Z]+\\.)?(${Object.keys(handlers).join('|')})$`);
 
-const options = {}, files = [];
+const options = {...defaultOptions};
+const files = [];
 
+// Parse arguments.
 for (let i = 0; i < args.length; i++) {
   const arg = args[i];
   if (!arg) continue;
@@ -41,12 +83,42 @@ for (let i = 0; i < args.length; i++) {
       i++;
     }
   } else if (fileMatch) {
-    let [, fileName, ext] = fileMatch;
-    fileName = fileName ? fileName.slice(0, fileName.length - 1) : 'index';
-    files.push({ fileName, ext });
+    let [, filename, ext] = fileMatch;
+    filename = filename ? filename.slice(0, filename.length - 1) : 'index';
+    files.push({ filename, ext });
   } else {
     throw new Error('Invalid option ' + arg);
   }
 }
 
-console.log(options, files);
+if (files.length === 0) {
+  files.push({ filename: 'index', ext: 'html' });
+}
+
+const htmlFiles = files.filter(isHtmlFile);
+const indexHtml = htmlFiles.find(isIndexFile);
+
+const resourceFileResults = files
+  .filter(negate(isHtmlFile))
+  .map(file => handlers[file.ext](options, file.filename + '.' + file.ext));
+console.log(resourceFileResults);
+
+const htmlFileResults = htmlFiles
+  .filter(negate(isIndexFile))
+  .map(file =>
+    handlers.html(options, file.filename + '.html', indexHtml ? undefined : resourceFileResults));
+
+if (indexHtml) {
+  htmlFileResults.push(handlers.html(options, 'index.html', resourceFileResults));
+}
+
+const promises = resourceFileResults
+  .concat(htmlFileResults)
+  .map(result => {
+    const pathname = path.join(options.dir, result.filename);
+    return writeFile(result.template, pathname).then(() => pathname);
+  });
+
+Promise.all(promises).then(pathnames => {
+  console.log('Done! These files were created:\n' + pathnames.join('\n'));
+});
